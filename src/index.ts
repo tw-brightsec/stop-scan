e9f9d4ac0873e854c9c75922790ecfc315a701f2
UncleVic <vic.polyakov@gmail.com>
2021-09-23 11:00:00 +0300

    fix: allow setting a custom hostname

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const apiToken = core.getInput("api_token");
 const scanId = core.getInput("scan");
 const hostname = core.getInput("hostname");
 
-const baseUrl = hostname ? `https://$hostname` : "https://nexploit.app";
+const baseUrl = hostname ? `https://${hostname}` : "https://nexploit.app";
 let restc: rm.RestClient = new rm.RestClient("GitHub Actions", baseUrl);
 
 async function stopScan(uuid: string) {
@@ -14,25 +14,9 @@ async function stopScan(uuid: string) {
       additionalHeaders: { Authorization: `Api-Key ${apiToken}` },
     };
     let restRes = await restc.get(`api/v1/scans/${uuid}/stop`, options);
-
-    switch (restRes.statusCode) {
-      case 200: {
-        core.info("Was succesfully stopped");
-        break;
-      }
-      case 401: {
-        core.setFailed("Failed to log in with provided credentials");
-        break;
-      }
-      case 403: {
-        core.setFailed(
-          "The account doesn't have any permissions for a resource"
-        );
-        break;
-      }
-    }
-  } catch (err) {
-    console.debug("Can't stop the scan");
+    core.info("Was succesfully stopped");
+  } catch (err: any) {
+    core.setFailed(`Failed (${err.statusCode}) ${err.message}`);
   }
 }
 
